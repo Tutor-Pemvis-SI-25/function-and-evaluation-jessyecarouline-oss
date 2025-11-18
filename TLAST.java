08ac97062ee9716dfb692291694fb94ef780f083
jessyecarouline <jessyecarouline@gmail.com>
2025-11-18 21:19:03 +0700

    JESSYE

--- TLAST.java
+++ TLAST.java
@@ -1,2 +1,15 @@
-//Nama - NIM
+//Jessye - 12S25054
+import java.util.*;
+import java.lang.Math;
+
+public class TLAST {
+    public static void main(String[] args) {
+        int max, jlhdata;
+
+        max = 10;
+        jlhdata = 0;
+        // Type Name;
+
+    }
+}
 
